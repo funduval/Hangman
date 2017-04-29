e928be70e8c1183468bd117d02aa03f0cf1db46a
funduval <fundaduval@gmail.com>
2017-04-29 18:07:56 -0400

    need to lock letters and clear at endgame

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,7 @@
+//Start the game with Click event that tells rules of the game and populated the guess field
+
+
+
 $("#start").on("click", function() {
 
 
@@ -7,12 +11,17 @@ $("#start").on("click", function() {
 
 
 
+//Create Array of words
 
 
+var dict = ["MARE", "DUEL", "GOLDRUSH", "QUARTZ", "SHOTGUN", "PYRITE", "GOLDMINE", "PISTOL", "WYOMING", "CAVERN", "COWGIRL", "STOCKADE", "WILD"];
 
-var dict = ["ZERO", "MOUNTAIN", "GOLDRUSH", "QUARTZ", "SHOTGUN", "PYRITE", "GOLDMINE", "PISTOL", "WYOMING", "SADDLE", "COWBOY", "STOCKADE", "BARKEEP"];
+
+//CREATE A RANDOMIZED WAY TO CHOOSE AN INDEX
 
 var i = Math.floor(Math.random() * dict.length)
+
+//create variables for the word chosen, a new array made of the word's letters, and array made of guessed letters, and an array made of successful guesses
 
 var word = dict[i];
 
@@ -22,7 +31,9 @@ var guessArray = [];
 
 var goodGuess = [];
 
-
+//create a loop that iterates through every index of the word itself and do several things:
+//push each letter into an array holding them like separate string items
+//create a paragraph element for each letter and give it the class "placeholder" and push text (underscore) into each new p 
 
 for (count = 0; count < word.length; count += 1) {
 
@@ -39,12 +50,17 @@ for (count = 0; count < word.length; count += 1) {
     letterHolder.text(wordArray[count]);
 
 
-    $(".compGuess").append(" _ ");
+    // $(".compGuess").append(" _ ");    ---->   if all else fails use this
 
 }
 
+    var wins = 0;
+    var lossCounter = 7;
 
-
+//when a key is clicked in the input field it does several things
+//the event is stored as a variable
+//a new variable is created that makes it upper case
+//each letter is pushed into a brand new array that holds all the guesses
 
 document.onkeyup = function(event) {
 
@@ -52,38 +68,80 @@ document.onkeyup = function(event) {
     var letter = userGuess.toUpperCase();
     guessArray.push(letter);
 
-
+//a test is performed to see if the letter is even in the array of letters for the computer's word
+//this is tested by seeing if indexOf returns -1
+//if it doesn't return -1, that means it's in the word and scores a win & affirmations are given to user
+//or else it records a loss, which is a countdown from 7--the established number in the beginning & warnings are issued to user
 
     if (wordArray.indexOf(guessArray[guessArray.length - 1]) != -1) {
 
+                $("#start").html("You got somethin' right!!");
 
-        $("#start").html("You got somethin' right!!");
+                wins++;
+                goodGuess.push(letter);
 
-        var wins = wins++;
-        goodGuess.push(letter);
+                //inside this condition we must see if we've reached maximum goodnes by comparing good guesses string to the word array indices
 
+                function winTest() {
 
-    } else if (wordArray.indexOf(guessArray[guessArray.length - 1]) == -1) {
+                        if (goodGuess.length === word.length) {
+
+                        $("#start").html("You won!! Your word was " + word + ".");
+
+                        }
+                    }
+
+                winTest();
+    } 
+
+    if (wordArray.indexOf(guessArray[guessArray.length - 1]) == -1) {
 
         $("#start").html("Yer one step closer to yer maker");
 
-        var losses = losses++;
+     lossCounter--;
 
+//inside this condition we must see if we've reached maximum losses by comparing countDown number to 7, our starting point
 
-    } else if (goodGuess.length === word.length) {
+                    function lossTest() {
 
-        $("#start").html("You won!! Your word was " + word + ".");
+                        if (lossCounter == 0) {
 
-    } else {
+                        $("#start").html("Yer hanged, man!");
 
-        $("#start").html("Yer hanged, man!");
+                        }
+
+                    }
+
+        lossTest();
 
     }
 
-
-
-
 }
+
+
+    
+
+
+
+
+
+
+
+   
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
